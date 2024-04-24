05c0af9951cfa7401604bf6400f22b6326f2ce14
Oleksii <sidone666@gmail.com>
2024-04-24 17:56:43 +0300

    format files

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -6,50 +6,49 @@ import { reactive } from 'vue';
 
 const router = useRouter();
 
+
 const formData = reactive({
-  email: '',
-  password: '',
+    email: '',
+    password: '',
 })
 
 const rules = {
-  email: { required, email },
-  password: { required },
+    email: { required, email },
+    password: { required },
 }
 
 const v$ = useVuelidate(rules, formData)
 
 const submitForm = async () => {
-  const result = await v$.value.$validate()
+    const result = await v$.value.$validate()
 
-  if (!v$.value.$error) {
-    router.push('/game')
-  } else {
-    alert('Form must be correct')
-  }
+    if (!v$.value.$error) {
+        router.push('/game')
+    } else {
+        alert('Form must be correct')
+    }
 }
 </script>
 
 
 <template>
-  <v-container class="fill-height">
+<v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
-      <v-row class="d-flex align-center justify-center">
-        <v-card title="Form" width="400" class="py-4">
-          <v-sheet class="mx-auto" width="300">
-            <v-form @submit.prevent="submitForm">
-              <v-text-field @blur="v$.email.$touch" @input="v$.email.$touch" v-model="formData.email" label="E-mail"
-                :error-messages="v$.email.$errors.map(e => e.$message)"></v-text-field>
-              <v-text-field @blur="v$.password.$touch" @input="v$.password.$touch" v-model="formData.password" label="Password" type="password"
-                :error-messages="v$.password.$errors.map(e => e.$message)"></v-text-field>
-              <v-row>
-                <v-col>
-                  <v-btn type="submit" color="indigo">login</v-btn>
-                </v-col>
-              </v-row>
-            </v-form>
-          </v-sheet>
-        </v-card>
-      </v-row>
+        <v-row class="d-flex align-center justify-center">
+            <v-card title="Form" width="400" class="py-4">
+                <v-sheet class="mx-auto" width="300">
+                    <v-form @submit.prevent="submitForm">
+                        <v-text-field @blur="v$.email.$touch" @input="v$.email.$touch" v-model="formData.email" label="E-mail" :error-messages="v$.email.$errors.map(e => e.$message)"></v-text-field>
+                        <v-text-field @blur="v$.password.$touch" @input="v$.password.$touch" v-model="formData.password" label="Password" type="password" :error-messages="v$.password.$errors.map(e => e.$message)"></v-text-field>
+                        <v-row>
+                            <v-col>
+                                <v-btn type="submit" color="indigo">login</v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-form>
+                </v-sheet>
+            </v-card>
+        </v-row>
     </v-responsive>
-  </v-container>
+</v-container>
 </template>
